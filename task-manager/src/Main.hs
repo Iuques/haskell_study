5d3252a92ed246d2972b825de6f456b268bdb0dd
Iuques <pedrolucassbb@gmail.com>
2024-10-09 21:12:40 -0300

    Melhorias na Main

--- task-manager/src/Main.hs
+++ task-manager/src/Main.hs
@@ -1,5 +1,54 @@
 module Main (main) where
 
+import System.Process (callCommand)
+import Task (Task)
+import TaskManager ( createTask, addTask, printTaskList, markComplete )
+
+-- Função para mostrar o menu e receber a escolha do usuário
+menu :: [Task] -> IO ()
+menu taskList = do
+    clearScreen
+    putStrLn "Task Manager - Escolha uma opção:"
+    putStrLn "1. Criar nova task"
+    putStrLn "2. Visualizar tasks"
+    putStrLn "3. Marcar task como concluída"
+    putStrLn "4. Sair"
+    option <- getLine
+    handleOption (read option :: Int) taskList
+
+-- Função que age de acordo com a opção escolhida
+handleOption :: Int -> [Task] -> IO ()
+handleOption 1 taskList = do
+    let taskId = length taskList + 1
+    putStrLn "Digite o nome da task:"
+    taskName <- getLine
+    putStrLn "Digite a descrição da task:"
+    taskDesc <- getLine
+    let task = createTask taskId taskName taskDesc False
+    putStrLn $ "Task criada: " ++ show task
+    let newTaskList = addTask task taskList
+    menu newTaskList -- Volta ao menu principal após criar a task
+handleOption 2 taskList = do
+    putStrLn "Visualizando tasks..."
+    printTaskList taskList
+    putStrLn "Pressione Enter para voltar ao menu."
+    _ <- getLine
+    menu taskList -- Volta ao menu principal
+handleOption 3 taskList = do
+    putStrLn "Digite o ID da task a ser marcada como concluída:"
+    taskId <- getLine
+    let newTaskList = markComplete (read taskId) taskList
+    putStrLn $ "Task com ID " ++ taskId ++ " marcada como concluída!"
+    menu newTaskList -- Volta ao menu principal
+handleOption 4 _ = do
+    putStrLn "Saindo do programa."
+handleOption _ taskList = do
+    putStrLn "Opção inválida, tente novamente."
+    menu taskList
+
+clearScreen :: IO()
+clearScreen = do
+  callCommand "clear" -- Para Linux e macOS
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = menu []
--- task-manager/src/TaskManager.hs
+++ task-manager/src/TaskManager.hs
@@ -1,4 +1,4 @@
-module TaskManager (addTask, rmvTask, markComplete) where
+module TaskManager (addTask, rmvTask, markComplete, createTask, printTaskList) where
 
 import Task
 
@@ -10,3 +10,22 @@ rmvTask i = filter (\t -> taskId t /= i)
 
 markComplete :: Int -> [Task] -> [Task]
 markComplete i = map (\t -> if taskId t == i then t {completed = True} else t)
+
+createTask :: Int -> String -> String -> Bool -> Task
+createTask id_ name_ description_ status = Task { taskId = id_, name = name_, description = description_, completed = status}
+
+printTask :: Task -> IO()
+printTask task = do
+    putStrLn ("ID: " ++ show (taskId task))
+    putStrLn ("Nome: " ++ name task)
+    putStrLn ("Descrição: " ++ description task)
+    putStr "Status: "
+    if completed task
+        then putStrLn "Completo"
+    else putStrLn "Incompleto"
+
+printTaskList :: [Task] -> IO()
+printTaskList taskList = do
+    mapM_ (\task -> do
+        printTask task
+        putStrLn "---------------") taskList
